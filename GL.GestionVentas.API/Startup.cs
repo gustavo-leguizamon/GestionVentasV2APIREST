e8867395ed2d0354b2406eb887a7b942b808794a
Gustavo <Hukummi@live.com>
2020-05-03 22:29:27 -0300

    Add swagger

--- GL.GestionVentas.API/Startup.cs
+++ GL.GestionVentas.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace GL.GestionVentas.API
 {
@@ -24,12 +25,38 @@ namespace GL.GestionVentas.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GestionVentas APIs v1.0",
+                    Description = "Test services"
+                });
+            });
+
+
+
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GestionVentas APIs v1.0");
+                c.RoutePrefix = string.Empty;
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
